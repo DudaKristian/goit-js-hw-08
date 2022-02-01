cb5001ba061674dbef38f79a4a25a8c5c8194074
Christian Duda <duda.kristian.ns@gmail.com>
2022-02-01 19:29:08 +0200

    Task 1 finished

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galeryListEl = document.querySelector('.gallery');
+
+const makeGalery = makeGaleryList(galleryItems);
+
+function makeGaleryList(array) {
+  return array
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href='${original}'>
+        <img
+          class="gallery__image"
+          src='${preview}'
+          alt='${description}'
+          
+        />
+      </a>
+    `;
+    })
+    .join('');
+}
+
+galeryListEl.insertAdjacentHTML('beforeend', makeGalery);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: `alt`,
+  captionDelay: 250,
+});
