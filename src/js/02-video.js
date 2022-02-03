69a5fda2fc6e14d11d305494e154424310138628
Christian Duda <duda.kristian.ns@gmail.com>
2022-02-03 15:30:26 +0200

    finished

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const TIME_KEY = 'videoplayer-current-time';
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem(TIME_KEY, data.seconds);
+  }, 1000),
+);
+player
+  .setCurrentTime(localStorage.getItem(TIME_KEY))
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
